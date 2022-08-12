d55ceeedae774f000fc0b6cf812fa89c08be02bc
Dilkin-hub <gdilkin46@gmail.com>
2022-08-11 22:11:11 -0300

    Primeiro envio da função fizzbuzz

--- fizzbuzz.php
+++ fizzbuzz.php
@@ -1,1 +1,18 @@
+<?php //Hi, this is the Fizzbuzz challenge
 
+function FizzBuzz (int $number)
+{
+    if ($number % 15 == 0) :
+        echo 'FizzBuzz';
+    else :
+        if ($number % 5 == 0) {
+            echo 'Buzz';
+        } else {
+            if ($number % 3 == 0) {
+                echo 'Fizz';
+            } else {
+                echo $number;
+            }
+        }
+    endif;
+};
