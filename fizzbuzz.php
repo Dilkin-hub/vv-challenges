0abde63c592472651fd870f39837665926656fb2
Guilherme Dilkin <75806348+Dilkin-hub@users.noreply.github.com>
2022-08-12 08:36:02 -0300

    Update fizzbuzz.php

--- fizzbuzz.php
+++ fizzbuzz.php
@@ -7,15 +7,15 @@ function FooBarFizz (int $number)
     else :
 	if ($number % 7 == 0) {
 		echo 'Fizz ';
-        if ($number % 5 == 0) {
-            echo 'Bar ';
-        } else {
-            if ($number % 3 == 0) {
-                echo 'Foo ';
-            } else {
-                echo $number;
-            }
-        }
+		if ($number % 5 == 0) {
+		    echo 'Bar ';
+		} else {
+		    if ($number % 3 == 0) {
+			echo 'Foo ';
+		    } else {
+			echo $number;
+		    }
+		}
 	}
     endif;
 };
