47cebd35aa4f8b8893281280056aa41664fdff48
Guilherme <gdilkin@yahoo.com.br>
2022-08-12 08:45:02 -0300

    Correção da função

--- fizzbuzz.php
+++ fizzbuzz.php
@@ -1,1 +1,25 @@
+<?php
 
+function FooBarFizz (int $num1, int $num2)
+{
+	if (!$num1 && !$num2) return;
+	
+	$nums = [$num1, $num2];
+	
+    if ($number % 15 == 0) :
+        echo 'FooBar';
+    else :
+        if ($number % 7 == 0) :
+            echo 'Fizz ';
+            if ($number % 5 == 0) :
+                echo 'Bar ';
+            else :
+                if ($number % 3 == 0) :
+                    echo 'Foo ';
+                else :
+                    echo $number;
+                endif;
+            endif;
+        endif;
+    endif;
+};
