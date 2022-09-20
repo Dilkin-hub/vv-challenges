6981e83291d2bd2ba71f09773b4cbdc831e42cb4
Dilkin-hub <gdilkin46@gmail.com>
2022-09-20 12:36:24 -0300

    Finalização de login e início pág. de jogo

---
+++ memory-game/js/main.js
@@ -0,0 +1,21 @@
+const playerInput = document.querySelector('.player-name-input');
+const formButton  = document.querySelector('.send-player-btn');
+const form        = document.querySelector('#memory_game_login');
+
+const validatePlayer = ({target}) => {
+    if (target.value.length > 3) {
+        formButton.removeAttribute('disabled');
+        return;
+    }
+
+    formButton.setAttribute('disabled', '');
+}
+
+const onSubmit = (event) => {
+    event.preventDefault();
+    localStorage.setItem('player 1', playerInput.value);
+    window.location = 'steps/game.html';
+}
+
+playerInput.addEventListener('input', validatePlayer);
+form.addEventListener('submit', onSubmit);
