f4a488718df7c4f86f6469ee22628822d11162ad
Dilkin-hub <gdilkin46@gmail.com>
2023-06-07 00:58:44 -0300

    Desafio de adivinhação

---
+++ guessing-game/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <link rel="stylesheet" href="./css/main.css">
+
+  <script defer src="./js/main.js"></script>
+
+  <title>Jogo de adivinhação</title>
+</head>
+
+<body>
+
+  <div class="guessing-bg">
+    <div class="guessing-main-container">
+      <div class="container-header">
+        <h1 class="guessing-title">Jogo de Adivinhação</h1>
+        <span class="guessing-description">
+          Clique no botão abaixo para iniciar. O sistema gerará um número aleatório e você deverá adivinhá-lo.
+        </span>
+      </div>
+      <div class="container-main">
+        <a class="generate-number-btn">
+          Gerar número
+        </a>
+      </div>
+      <div class="container-footer"></div>
+    </div>
+  </div>
+
+</body>
+
+</html>
